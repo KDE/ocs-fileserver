46b01b82e896dd94dafa216b9ece10918d334a10
Ronald von Schlegell <rvs75@email.de>
2019-12-04 16:14:26 +0100

    toc

--- api_application/controllers/Files.php
+++ api_application/controllers/Files.php
@@ -1344,7 +1344,7 @@ class Files extends BaseController
             }
             
             
-            if(is_dir($dir.'/'.$ff)) $this->listFolderFiles($dir.'/'.$ff, $fileNameList);
+            if(is_dir($dir.'/'.$ff)) $fileNameList = $this->listFolderFiles($dir.'/'.$ff, $fileNameList);
         }
         
         return $fileNameList;

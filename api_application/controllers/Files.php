1e676b348fcf2444c1fe63188670d2f4e84c56fd
Alexander Schmidt <2f5ed1c5@opayq.com>
2020-01-15 11:46:43 +0100

    updated Files.php

--- api_application/controllers/Files.php
+++ api_application/controllers/Files.php
@@ -1174,6 +1174,7 @@ class Files extends BaseController
             // Redirect to opendesktop project page
             //$this->response->redirect($this->appConfig->general['redirectTargetServer'] . '/co/' . $collectionId);
             $this->response->setStatus(410);
+            $this->_setResponseContent('error');
             //throw new Flooer_Exception('Link expired', LOG_NOTICE);
         }
     }

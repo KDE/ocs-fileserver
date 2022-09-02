084686bf472d762913e7c10f0e88d4922cdd5105
Alexander Schmidt <2f5ed1c5@opayq.com>
2022-09-02 13:21:29 +0200

    updated waveform
    (cherry picked from commit cb9299f001dab7d33b69f89ec8a2e23898b2d677)

---
+++ api_application/controllers/Waveform.php
@@ -0,0 +1,84 @@
+<?php /** @noinspection PhpUndefinedFieldInspection */
+
+/**
+ * file server - part of Opendesktop.org platform project <https://www.opendesktop.org>.
+ *
+ * Copyright (c) 2016 pling GmbH.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+class Waveform extends BaseController
+{
+
+    /**
+     * @throws Flooer_Exception
+     */
+    public function getFile()
+    {
+        $id = null;
+
+        if (!empty($this->request->id)) {
+            $id = $this->request->id;
+        }
+
+        $file = $this->models->files->getFile($id);
+
+        // check file pre-conditions
+        if (!$file) {
+            $this->response->setStatus(404);
+            throw new Flooer_Exception('Not found', LOG_NOTICE);
+        }
+        if (!$file->active) {
+            $this->log->log("File is inactive (file: $file->id)", LOG_NOTICE);
+            $this->response->setStatus(404);
+            throw new Flooer_Exception('Not found', LOG_NOTICE);
+        }
+        if (substr( $file->type, 0, 5 ) !== "audio") {
+            $this->response->setStatus(404);
+            throw new Flooer_Exception('Not an audio file (' . $file->id . ')', LOG_NOTICE);
+        }
+
+        $collectionId = $file->collection_id;
+        $collection = $this->models->collections->$collectionId;
+
+        // check collection pre-conditions
+        if (!$collection) {
+            $this->log->log("Collection not found (file: $file->id)", LOG_NOTICE);
+            $this->response->setStatus(404);
+            throw new Flooer_Exception('Not found', LOG_NOTICE);
+        }
+        if (!$collection->active) {
+            $this->log->log("Collection is inactive (file: $file->id)", LOG_NOTICE);
+            $this->response->setStatus(404);
+            throw new Flooer_Exception('Not found', LOG_NOTICE);
+        }
+
+        $collectionDir = $this->appConfig->general['filesDir'] . '/' . $collection->name;
+        $filePath = $collectionDir . '/' . $file->name;
+        $filePathTarget = $filePath . '.json';
+
+        if (!file_exists($filePathTarget)) {
+            $this->_generateWaveForm($filePath, $filePathTarget);
+        }
+
+        $fileName = $file->name . '.json';
+        $fileType = 'application/json';
+        $fileSize = filesize($filePathTarget);
+
+        $this->_sendFile($filePathTarget, $fileName, $fileType, $fileSize, true, false);
+    }
+
+
+}
